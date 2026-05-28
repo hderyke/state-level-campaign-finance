79855178e9104021aa289cff82ff9ac37637123e
Henry <leaguejoeredditbot@gmail.com>
2026-05-28 21:27:38 +0000

    restore default user agent with update instructions

--- config.py
+++ config.py
@@ -3,7 +3,8 @@
 # --- http ---
 
 # Spoofs a standard browser to avoid 403s from state disclosure sites.
-# Update if requests start getting blocked (use a current Chrome UA string).
+# Update this to match your own browser/OS if you start getting blocked.
+# Find your user agent at: https://www.whatismybrowser.com
 USER_AGENT = (
 # change this to whatever browser you are using for scraping
 )
